e6bb8d50ba5652248c645f06f47fbb349fc9978c
Renato Guerato <renato-guerato15@hotmail.com>
2025-01-24 19:50:48 -0400

    Montando função para agregar nomes.

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let listaNomes = [];
+
+function adicionarNome() {
+    let nome = document.getElementById("amigo").value;
+    if (nome === "") {
+        alert("Por favor, insira um nome.");
+        return;
+    } else {
+        listaNomes.push(nome);
+        document.getElementById("amigo").value = "";
+        ;
+    }
+}
